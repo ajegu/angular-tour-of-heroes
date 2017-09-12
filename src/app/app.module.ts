1c8cb641254f87ecb62f6a6f5548cb4bd26e94dd
Allan JEGU <allanjegu@gmail.com>
2017-09-12 17:50:05 +0200

    angular tutorial - routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
+import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }

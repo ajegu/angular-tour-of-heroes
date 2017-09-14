453021d7b6a7f09082a1cbe93ab56814a0dd00ea
Allan JEGU <allanjegu@gmail.com>
2017-09-14 13:05:13 +0200

    http

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -16,7 +16,8 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n   <h1>{{title}}</h1>\n   <my-heroes></my-heroes>\n "
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
